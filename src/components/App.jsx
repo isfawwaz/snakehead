c0e491d133ccba5be78729efa965de14ef5e39d5
Ahmad Faris <isfawwaz@gmail.com>
2021-02-19 16:15:09 +0700

    feat(init): setup base route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,14 @@
+import { Route, Switch } from 'react-router-dom';
+import Home from './../pages/Home';
+
 export default function App() {
-    return <main className="app"></main>
+    return <main className="app">
+        <section className="sh-main-sidebar"></section>
+        <section className="sh-main-content">
+            <Switch>
+                <Route path="/" exact component={ Home } />
+            </Switch>
+        </section>
+        <section className="sh-main-detail"></section>
+    </main>
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './utils/reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import './styles/styles.scss';
 
 ReactDOM.render(
     <React.StrictMode>
-        <ChakraProvider>
-            <App />
-        </ChakraProvider>
+        <BrowserRouter>
+            <ChakraProvider>
+                <App />
+            </ChakraProvider>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
